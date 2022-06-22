32abb9b3834071051b617254036bdf23e373bddf
Felipe14126k <103604030+Felipe14126k@users.noreply.github.com>
2022-06-22 18:10:45 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,74 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
+var engine,world;
+
+var ground
+var ball
+var ground1
+var ground2
+
+function preload() {
+
 }
 
 function setup() {
 	createCanvas(800, 700);
-
-
 	engine = Engine.create();
-	world = engine.world;
+	world = engine.world; 
 
-	//Crie os Corpos Aqui.
+	var ground_options ={
+		isStatic: true
+	  };
+	 
+   
+	 ground = Bodies.rectangle(299,711,1000,10,ground_options);
+	 World.add(world,ground);
+	
+	var ball_options = {
+		resititution :0.5,
+		friction:0.05,
+		frictionAir:0
+	}
+	
+	var ground1_options = {
+		resititution :0.7,
+		friction:0.01,
+		frictionAir:0.1
+	}
 
+	//var ground2_options = {
+		//resititution :0.1,
+		//friction:1,
+		//frictionAir:0.3
+	//}
+	
+	ball = Bodies.circle(220,10,10,ball_options);
+	World.add(world,ball);
 
-	Engine.run(engine);
-  
+    ground1 = Bodies.rect(110,50,10,10);
+	 World.add(world,ground1);
+
+	// block3 = Bodies.rectangle(350,50,10,10);
+	// World.add(world,block3);
+
+	rectMode(CENTER)
+	ellipseMode(RADIUS)
 }
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+	rectMode(CENTER);
+	background('LIGHTGREEN');
+
+	drawSprites();
+
+	Engine.update(engine)
+
+	ellipse(ball.position.x,ball.position.y,20);
+	rect(ground.position.x,ground.position.y,1000,20);	 
 }
 
 
